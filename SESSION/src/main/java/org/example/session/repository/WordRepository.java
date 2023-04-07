58a928a2bdcae05fdd1ba76697e50073065d46a9
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-04-07 18:53:51 +0200

    [SESSION] Define count by language id

--- SESSION/src/main/java/org/example/session/repository/WordRepository.java
+++ SESSION/src/main/java/org/example/session/repository/WordRepository.java
@@ -1,9 +1,13 @@
 package org.example.session.repository;
 
 import org.example.session.entity.WordEntity;
+import org.example.session.entity.ids.WordId;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface WordRepository extends ReactiveCrudRepository<WordEntity, Integer> {
+public interface WordRepository extends ReactiveCrudRepository<WordEntity, WordId> {
+
+	Mono<Long> countAllByLanguageId(int languageId);
 }
