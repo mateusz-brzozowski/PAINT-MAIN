273b1108f19854e35050a32991cb4648ace3dbc1
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-04-07 18:53:34 +0200

    [SESSION] Update wordle controller implementation

--- SESSION/src/main/java/org/example/session/controller/WordleController.java
+++ SESSION/src/main/java/org/example/session/controller/WordleController.java
@@ -1,9 +1,14 @@
 package org.example.session.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.session.model.Session;
+import org.example.session.model.WordleResult;
 import org.example.session.service.WordleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +22,18 @@ public class WordleController {
 	private final WordleService service;
 
 	@GetMapping
-	public Mono<String> ping(@RequestParam(required = false) String name) {
-		return Mono.justOrEmpty(name)
-				.defaultIfEmpty("World")
-				.map(it -> String.format("Hello, %s!", it))
-				.flatMap(it -> service.test());
+	public Mono<Session> initializeSession(
+			@RequestParam Integer languageId,
+			@RequestHeader(required = false, defaultValue = "5") Integer wordLength
+	) {
+		// TODO: implement different modes
+		var userId = 1; // TODO: retrieve user id from provided token
+		return service.initializeSession(userId, languageId, wordLength);
+	}
+
+	@PostMapping
+	public Mono<WordleResult> guess(@RequestHeader Integer sessionId, @RequestBody String guess) {
+		var userId = 1; // TODO: retrieve user id from provided token
+		return service.handleGuess(userId, sessionId, guess);
 	}
 }
--- SESSION/src/main/java/org/example/session/service/implementation/WordleServiceImpl.java
+++ SESSION/src/main/java/org/example/session/service/implementation/WordleServiceImpl.java
@@ -3,13 +3,15 @@ package org.example.session.service.implementation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.session.entity.SessionEntity;
-import org.example.session.entity.WordEntity;
+import org.example.session.mapper.SessionMapper;
+import org.example.session.model.Session;
+import org.example.session.model.WordleResult;
+import org.example.session.repository.GuessRepository;
 import org.example.session.repository.SessionRepository;
 import org.example.session.repository.WordRepository;
 import org.example.session.service.WordleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -17,16 +19,24 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class WordleServiceImpl implements WordleService {
 
-	private final SessionRepository sessionRepository;
 	private final WordRepository wordRepository;
+	private final SessionRepository sessionRepository;
+	private final GuessRepository guessRepository;
+
+	private final SessionMapper sessionMapper;
 
 	@Override
-	public Mono<String> test() {
-		var session = sessionRepository.save(SessionEntity.builder().build());
-		var word = wordRepository.save(WordEntity.builder().build());
-		return Flux.concat(
-				session,
-				word
-		).reduce("", (a, b) -> a + b.toString());
+	public Mono<Session> initializeSession(int userId, int languageId, int wordLength) {
+		var word = wordRepository.countAllByLanguageId(languageId);
+		var session = SessionEntity.builder()
+				.userId(userId)
+				.wordLength(wordLength)
+				.build();
+		return null;
+	}
+
+	@Override
+	public Mono<WordleResult> handleGuess(int userId, int sessionId, String guess) {
+		return null;
 	}
 }
