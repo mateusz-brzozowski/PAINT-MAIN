390d9481e126a9f99f02188afbcbc0262ac2ec8e
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2023-04-12 21:43:18 +0200

    Add a basic keyboard component skeleton

---
+++ FE/app.js
@@ -0,0 +1,20 @@
+const tileDisplay = document.querySelector(".tile-container")
+const keyboard = document.querySelector(".key-container")
+
+const keys = [
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', "ENTER",
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M', '≪'
+]
+
+const handleClick = () => {
+    console.log("Click!")
+}
+
+keys.forEach(key => {
+    const buttonElement = document.createElement("button")
+    buttonElement.textContent = key
+    buttonElement.setAttribute("id", key)
+    buttonElement.addEventListener("click", handleClick)
+    keyboard.append(buttonElement)
+})
