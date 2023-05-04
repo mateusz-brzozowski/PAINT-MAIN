489e0e40da0fe4d794b58f225b2e32721ed3d06a
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-05-05 00:27:22 +0200

    Changed word_number values

--- DATABASE/data/from_file_inserts.py
+++ DATABASE/data/from_file_inserts.py
@@ -7,11 +7,13 @@ random.seed(10)
 def get_english_words(file_read):
     # words source: https://copylists.com/words/list-of-4-letter-words/
     file = ""
+    i=0
     with open(file_read, 'r') as fp:
         for line in fp.readlines():
             nr, word = line.split(',')
-            text = f"INSERT INTO words(word_id, language_id, word_length, word_number, word) VALUES ({int(nr)+1000}, 1, 4, 0, '{word[:-1]}');\n"
+            text = f"INSERT INTO words(word_id, language_id, word_length, word_number, word) VALUES ({int(nr)+1000}, 1, 4, {i}, '{word[:-1]}');\n"
             file+=text
+            i+=1
     return file
 
 def get_polish_words(file_read, nr_letters, start=2000, max_amount=500):
@@ -22,7 +24,7 @@ def get_polish_words(file_read, nr_letters, start=2000, max_amount=500):
         for line in fp.readlines():
             # random values in order to get more diverse list -> not only words starting with 'a'
             if (len(line[:-2]) == nr_letters and random.randint(0, 80)%80==0):
-                text = f"INSERT INTO words(word_id, language_id, word_length, word_number, word) VALUES ({start}, 2, {nr_letters}, 0, '{unidecode(line[:-2])}');\n"
+                text = f"INSERT INTO words(word_id, language_id, word_length, word_number, word) VALUES ({start}, 2, {nr_letters}, {i}, '{unidecode(line[:-2])}');\n"
                 file+=text
                 start+=1
                 i+=1
