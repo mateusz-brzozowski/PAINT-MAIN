f82dec2051836821828ae7b84b99aa84aa4885c4
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-05-21 14:09:55 +0200

    [SESSION] Allow for jwt authorization

---
+++ SESSION/src/main/java/org/example/session/config/CorsAllowOriginFilter.java
@@ -0,0 +1,19 @@
+package org.example.session.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CorsAllowOriginFilter implements WebFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		exchange.getResponse()
+				.getHeaders()
+				.add("Access-Control-Allow-Origin", "*");
+		return chain.filter(exchange);
+	}
+}
